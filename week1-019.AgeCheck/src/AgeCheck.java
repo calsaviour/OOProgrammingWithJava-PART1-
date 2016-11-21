feda652bca0d37d1a601de9487117f36e5cd8b41
Calvin Low <calsaviour@gmail.com>
2016-11-21 23:27:39 +0800

    Add Solution for Age Check

--- week1-019.AgeCheck/src/AgeCheck.java
+++ week1-019.AgeCheck/src/AgeCheck.java
@@ -5,6 +5,11 @@ public class AgeCheck {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        int age = Integer.parseInt(reader.nextLine());
+        if (age >= 0 && age <= 120) {
+            System.out.print("OK");
+        } else {
+            System.out.print("Impossible");
+        }
     }
 }
