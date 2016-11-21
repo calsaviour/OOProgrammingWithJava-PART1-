2227fc8a4313dbeda95cba39121cf2de20e0222a
Calvin Low <calsaviour@gmail.com>
2016-11-21 20:31:14 +0800

    Add solution for bigger number

--- week1-011.BiggerNumber/src/BiggerNumber.java
+++ week1-011.BiggerNumber/src/BiggerNumber.java
@@ -5,7 +5,21 @@ public class BiggerNumber {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        int firstInput, secondInput;
         // Implement your program here. Remember to ask the input from user
+        System.out.print("Type a number: ");
+        firstInput = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        secondInput = Integer.parseInt(reader.nextLine());
+
+        int biggerNumber;
+        if (firstInput > secondInput) {
+            biggerNumber = firstInput;
+        } else {
+            biggerNumber = secondInput;
+        }
+
+        System.out.print("The bigger number of the two numbers given was: " + biggerNumber);
+
     }
 }
