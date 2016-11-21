925172d50adb7fd46ccdb9ca9f44534cebdd14b4
Calvin Low <calsaviour@gmail.com>
2016-11-21 23:04:14 +0800

    Add solution for GradesAndPoints

--- week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -5,6 +5,24 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type the points [0-60]: ");
+        int points = Integer.parseInt(reader.nextLine());
+        if (0 <= points && points <= 29) {
+            result("failed");
+        } else if (30 <= points && points <= 34) {
+            result("1");
+        } else if (35 <= points && points <= 39) {
+            result("2");
+        } else if (40 <= points && points <= 44) {
+            result("3");
+        } else if (45 <= points && points <= 49) {
+            result("4");
+        } else if (50 <= points && points <= 60) {
+            result("5");
+        }
+    }
 
+    private static void result(String result) {
+        System.out.print("Grade" + result);
     }
 }
