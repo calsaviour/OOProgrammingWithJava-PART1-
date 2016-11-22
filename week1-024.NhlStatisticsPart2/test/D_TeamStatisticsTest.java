6a5b8510b09bde677648801fe2ec6a9e633a9521
Calvin Low <calsaviour@gmail.com>
2016-11-22 17:29:24 +0800

    Add solution for NhlStatistics Part 2

--- week1-024.NhlStatisticsPart2/test/D_TeamStatisticsTest.java
+++ week1-024.NhlStatisticsPart2/test/D_TeamStatisticsTest.java
@@ -2,10 +2,13 @@
 
 import fi.helsinki.cs.tmc.edutestutils.MockStdio;
 import fi.helsinki.cs.tmc.edutestutils.Points;
+
 import java.lang.reflect.Method;
 import java.util.List;
+
 import nhlstats.NHLStatistics;
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 @Points("24")
@@ -21,10 +24,10 @@ public class D_TeamStatisticsTest {
         io.setSysIn(input);
         NhlStatisticsPart2.main(null);
         input = input.replaceAll("\n", " ");
-        String viesti =  "With commands \""+input+"\" the statistics for Anaheim (ANA) sorted by points should be printed.";
-        
-        assertCalled(0, "sortByPoints", viesti+ " Remember to sort the players by points first");
-        assertCalled(1, "team ANA", viesti);       
+        String viesti = "With commands \"" + input + "\" the statistics for Anaheim (ANA) sorted by points should be printed.";
+
+        assertCalled(0, "sortByPoints", viesti + " Remember to sort the players by points first");
+        assertCalled(1, "team ANA", viesti);
 
     }
 
@@ -33,7 +36,7 @@ public class D_TeamStatisticsTest {
         assertTrue(msg, mt.size() > n);
         assertTrue(msg, mt.get(n).contains(method));
     }
-    
+
     private List<String> getMethodCalls() {
         try {
             Method m = NHLStatistics.class.getDeclaredMethod("calledMethods");
@@ -45,10 +48,10 @@ public class D_TeamStatisticsTest {
     }
 
     private void testMode() {
-        try {                        
+        try {
             Method m = NHLStatistics.class.getDeclaredMethod("testMode");
             m.setAccessible(true);
-            m.invoke(null);            
+            m.invoke(null);
         } catch (Throwable e) {
         }
     }
