e43936bd52017349455109a47d544fa48ca527a1
Calvin Low <calsaviour@gmail.com>
2016-12-05 17:42:01 +0800

    Add solution for Up to Certain number

--- week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -6,8 +6,15 @@ public class UpToCertainNumber {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         // Write your code here
-        
+        System.out.print("Up to what number?");
+        int number = reader.nextInt();
+        int i = 1;
+        while (i <= number) {
+            System.out.println(i);
+            i++;
+        }
+
     }
 }
