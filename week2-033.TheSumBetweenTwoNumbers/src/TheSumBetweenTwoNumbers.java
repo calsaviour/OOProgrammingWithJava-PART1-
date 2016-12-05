59de25d2ebcd03eabfcb2d386a1c4b812e95277e
Calvin Low <calsaviour@gmail.com>
2016-12-05 18:02:30 +0800

    Add solution for sum between two numbers

--- week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+        int firstNumber, secondNumber;
+        System.out.print("First: ");
+        firstNumber = reader.nextInt();
+        System.out.print("Last: ");
+        secondNumber = reader.nextInt();
+
+        int sum = 0;
+
+        while (firstNumber <= secondNumber) {
+            sum += firstNumber;
+            firstNumber++;
+        }
+
+        System.out.println("The sum " + sum);
     }
 }
