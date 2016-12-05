84521d0a87380be6ae77d0692329d0f915ffa638
Calvin Low <calsaviour@gmail.com>
2016-12-05 18:06:34 +0800

    Add solution for factorial

--- week2-034.Factorial/src/Factorial.java
+++ week2-034.Factorial/src/Factorial.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int number = reader.nextInt();
+        int factorial = 1;
+        while (number > 0) {
+            factorial *= number;
+            number--;
+        }
 
+        System.out.print("Factorial is " + factorial);
     }
 }
